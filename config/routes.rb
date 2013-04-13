7484b5f9a5b664cb09ee6dd5d6e87d4083d2f5e9
natew <natewienert@gmail.com>
2013-04-13 11:00:16 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,26 @@ require 'sidekiq/web'
 
 Read2::Application.routes.draw do
 
-  get "users/create"
-
-  get "users/update"
-
-  get "users/destroy"
-
-  get "users/activate"
-
   mount Sidekiq::Web => '/sidekiq'
 
   resources :feeds, only: [:show, :index]
+
+  # Users
+  resources :users do
+    member do
+      get :activate
+    end
+  end
+
+  match 'login': 'user_sessions#new', as: :login
+  match 'logout': 'user_sessions#destroy', as: :logout
+
+  resources :user_sessions, only: [:new, :create, :destroy]
+  resources :password_resets, only: [:create, :edit, :update]
+
+  resource :oauth do
+    get :callback
+  end
   
   root to: 'welcome#index'
 
